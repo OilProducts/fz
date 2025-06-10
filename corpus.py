10932d5ccb2d757fdc4d748cd11cd3fa54e88e42
Chris <christopher.l.charland@gmail.com>
2025-06-10 11:42:01 -0400

    Add debug logging and performance metric

--- corpus.py
+++ corpus.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import logging
 
 
 class Corpus:
@@ -13,10 +14,12 @@ class Corpus:
     def save_if_interesting(self, data, coverage):
         """Persist input if it triggers previously unseen coverage."""
         if not coverage - self.coverage:
+            logging.debug("Input did not yield new coverage")
             return False
         self.coverage.update(coverage)
         fname = hashlib.sha1(data).hexdigest()
         path = os.path.join(self.directory, fname)
         with open(path, "wb") as f:
             f.write(data)
+        logging.debug("Saved interesting input to %s", path)
         return True
